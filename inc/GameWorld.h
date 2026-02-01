809cbb2885798b7772e2dd88b5f621144af7986c
Pawelo12Amogus <kubao@betanet.pl>
2026-02-01 10:46:45 +0100

    implemented tile and tilemap classes

--- inc/GameWorld.h
+++ inc/GameWorld.h
@@ -4,16 +4,30 @@
 #include "Utils.h"
 #include "Player.h"
 
+
 class Tile
 {
     public:
         Tile() = default;
+        Tile(int i) : id(i){}
+        int id;
+        Player* owner = nullptr;  //ten tam ten pointer oznacza wlasciciela tego terytorium
+        std::unique_ptr<Building> building;
+        void CreateBuilding(std::unique_ptr<Building>&& building);
+        void DestroyBuilding();
+        void SetOwner(Player* player);
+        //bool fajnazgrabnanazwaczymoznazbudowac
+        bool CanBuild(Player* player);
 };
 
 class TileMap
 {
     public:
         TileMap() = default;
+        std::vector<Tile> tilemap;
+        Tile& GetTile(int id);//Tile pointer bo std::unique_ptr
+        void SetTile(int id, Tile&& tile);
+        void BuildOnTile(int id, Player* player, std::unique_ptr<Building>&& building);
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 // #include <iostream>
-#include "../raylib/include/raylib.h"
+//#include "../raylib/include/raylib.h"
 #include "Utils.h"
-
+#include "Game.h"
+#include "GameWorld.h"
 
 int main(void)
 {
     auto g = std::make_unique<Game>();
+    const auto tag = "[MAIN]";
+    Log::Msg(tag,"HELLO WHATEVER");
+    Player playering;
+    Player notsoplayering;
+    TileMap tilemap;
+    tilemap.tilemap.emplace_back(Tile{0});
+    tilemap.tilemap[0].SetOwner(&playering);
+    std::cout << tilemap.GetTile(0).building.get() << std::endl;
+    tilemap.BuildOnTile(0, &playering, std::make_unique<Building>());
+    std::cout << tilemap.GetTile(0).building.get() << std::endl;
+    /*Tile tile;
+    Player playering;
+    tile.CreateBuilding(std::make_unique<Building>());
+    std::cout << tile.building.get() << std::endl;
+    tile.DestroyBuilding();
+    std::cout << tile.building.get() << std::endl;
+    std::cout << tile.owner << std::endl;
+    tile.SetOwner(&playering);
+    std::cout << tile.owner << std::endl;
+    std::cout << tile.CanBuild(&playering) << std::endl;
+    */
     // g->InitGame();
     // g->GameLoop();
     // const int screenWidth = 800;
